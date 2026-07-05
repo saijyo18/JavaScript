ee5f3a5a1a32c1d20f6a23aef85b5423f7f9bb5f
saijyo18 <saijyothi1206@gmail.com>
2026-07-05 11:57:30 +0530

    practice

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-let para1  = document.createElement('p');
-para1.innerText = "Hey! I am red"
-document.querySelector('body').append(para1)
+let btns = document.querySelectorAll("button");
 
-para1.classList.add("red");
-
-let head1  = document.createElement('h3');
-head1.innerText = "Hey! I am blue"
-document.querySelector('body').append(head1)
-
-head1.classList.add("blue");
-
-let div = document.createElement('div')
-let h1 = document.createElement("h1");
-let para2 = document.createElement("p");
-
-h1.innerText = "Hello I am inside DIV";
-para2.innerText = "ME TOO";
-
-div.append(h1);
-div.append(para2);
-
-div.classList.add("box");
+function sayhello() {
+    alert("hello");
+}
 
 
-document.querySelector('body').append(div);
+for(btn of btns){
+    btn.onclick = sayhello;
+    btn.onmouseenter = function () {
+        console.log("You entered a button");
+    };
+    console.dir(btn);
+}
---
+++ app2.js
@@ -0,0 +1,28 @@
+let para1  = document.createElement('p');
+para1.innerText = "Hey! I am red"
+document.querySelector('body').append(para1)
+
+para1.classList.add("red");
+
+let head1  = document.createElement('h3');
+head1.innerText = "Hey! I am blue"
+document.querySelector('body').append(head1)
+
+head1.classList.add("blue");
+
+let div = document.createElement('div')
+let h1 = document.createElement("h1");
+let para2 = document.createElement("p");
+
+h1.innerText = "Hello I am inside DIV";
+para2.innerText = "ME TOO";
+
+div.append(h1);
+div.append(para2);
+
+div.classList.add("box");
+
+
+document.querySelector('body').append(div);
+
+//DOM EVENTS
