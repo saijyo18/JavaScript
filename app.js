fb077f7ccc81e9026c89a3889787a3db21a6f463
saijyo18 <saijyothi1206@gmail.com>
2026-07-06 12:06:52 +0530

    practice

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-let btns = document.querySelectorAll("button");
+// let btns = document.querySelectorAll("button");
 
-function sayhello() {
-    alert("hello");
-}
+// function sayhello() {
+//     alert("hello");
+// }
 
 
-for(btn of btns){
-    btn.onclick = sayhello;
-    btn.onmouseenter = function () {
-        console.log("You entered a button");
-    };
-    console.dir(btn);
-}
+// for(btn of btns){
+//     btn.onclick = sayhello;
+//     btn.onmouseenter = function () {
+//         console.log("You entered a button");
+//     };
+//     console.dir(btn);
+// }
+
+// EVENT LISTENER 
+// addeventlisterner
+
+
+// let btns = document.querySelectorAll("button");
+
+// for(btn of btns){
+//     // btn.onclick = sayhello;
+//     // btn.onclick = sayhi;
+//     btn.addEventListener("click", sayhello);
+//     btn.addEventListener("click", sayhi);
+
+// }
+// function sayhello() {
+//     alert("Hello");
+// }
+
+// function sayhi() {
+//     alert("apna college");
+// }
